dd6ec97d06c415471aedd63290df554bd6009357
jakub-g <jakub.g.opensource@gmail.com>
2014-03-07 13:49:05 +0100

    Extract perform checks

--- tasks/grunt-commit-message-verify.js
+++ tasks/grunt-commit-message-verify.js
@@ -6,7 +6,6 @@
  * Licensed under the Apache license.
  */
 var exec = require('child_process').exec;
-
 var checkCommit = require("../lib/check-commit-message");
 
 var shellCommand = "git log --format=%B --no-merges -n 1";
@@ -27,15 +26,6 @@ module.exports = function (grunt) {
             return false;
         }
 
-        cfg.regexes = cfg.regexes || [];
-        cfg.minLength = cfg.minLength || 0;
-        cfg.maxLength = cfg.maxLength || 0;
-
-        cfg.minFirstLineLength = cfg.minFirstLineLength || 0;
-        cfg.maxFirstLineLength = cfg.maxFirstLineLength || 0;
-
-        cfg.maxLineLength = cfg.maxLineLength || 0;
-
         var done = this.async();
         var childproc = exec(shellCommand, function (error, stdout, stderr) {
             if (error || stderr || this.errorCount) {
@@ -52,30 +42,14 @@ module.exports = function (grunt) {
 
             // the commit message, string with perhaps newlines
             var sMsg = stdout.trim();
-            var asMsg = sMsg.split("\n");
 
             // let's do the checks
-            var errors = [];
-            for (var regexName in cfg.regexes) {
-                var regexObj = cfg.regexes[regexName];
-                checkCommit.regex(sMsg, regexObj, regexName, errors);
-            }
-            var someRegexFailed = (errors.length > 0);
-
-            checkCommit.maxLineLength(asMsg, cfg.maxLineLength, errors);
-
-            checkCommit.checkLength(asMsg[0], {
-                min : cfg.minFirstLineLength,
-                max : cfg.maxFirstLineLength
-            }, "The first line of commit message", errors);
-
-            checkCommit.checkLength(sMsg, {
-                min : cfg.minLength,
-                max : cfg.maxLength
-            }, "The commit message", errors);
+            var report = checkCommit.performChecks(sMsg, cfg);
+            var errors = report.errors;
+            var someRegexFailed = report.someRegexFailed;
 
             // error reporting
-            var ok = (errors.length == 0);
+            var ok = (errors.length === 0);
             if (ok) {
                 grunt.log.writeln("The commit message was:".yellow);
                 grunt.log.writeln(sMsg);
@@ -96,9 +70,6 @@ module.exports = function (grunt) {
                 }
             }
             done(ok);
-
         });
-
     });
-
 };
